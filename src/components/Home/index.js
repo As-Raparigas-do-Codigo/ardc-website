f8a0707ff91b96a669c78570e17530084772c389
Pedro Fonseca <pmlfonseca@outlook.com>
2022-05-12 21:24:56 +0100

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,26 +4,26 @@ import React from "react";
 import Button from "../Button";
 
 function Home() {
-	return (
-		<div className="home">
-			<h1>Home Page</h1>
+    return (
+        <div className="home">
+            <h1>Home Page</h1>
 
-			<div class="gradient-zig-zag">
-				<Button btnClass="button-primary">Primary</Button>
-				<Button btnClass="button-primary" disabled>
-					Primary disabled
-				</Button>
-				<Button btnClass="button-secondary">Secondary</Button>
-				<Button btnClass="button-secondary" disabled>
-					Secondary disabled
-				</Button>
-				<Button btnClass="button-tertiary">Tertiary</Button>
-				<Button btnClass="button-tertiary" disabled>
-					Tertiary disabled
-				</Button>
-			</div>
+            <div class="gradient-zig-zag">
+                <Button btnClass="button-primary">Primary</Button>
+		<Button btnClass="button-primary" disabled>
+		    Primary disabled
+		</Button>
+		<Button btnClass="button-secondary">Secondary</Button>
+		<Button btnClass="button-secondary" disabled>
+		    Secondary disabled
+		</Button>
+		<Button btnClass="button-tertiary">Tertiary</Button>
+		<Button btnClass="button-tertiary" disabled>
+		    Tertiary disabled
+		</Button>
 		</div>
-	);
+	</div>
+    ;
 }
 
 export default Home;
