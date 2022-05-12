64a0a31d637836583cad409f8affd6a919cbd86a
Pedro Fonseca <pmlfonseca@outlook.com>
2022-05-12 21:26:09 +0100

    Update index.js

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,14 +4,14 @@ import React from "react";
 import { ReactComponent as GreenArrow } from "../../assets/icons/green-arrow.svg";
 
 export default function Button({ btnClass, href, disabled, children }) {
-    return btnClass === "button-primary" ? (
-        <button disabled={disabled} className={btnClass}>
-            {children}
-	 </button>
-    ) : (
-        <a href={href} disabled={disabled} className={btnClass}>
-            {btnClass === "button-tertiary" && <GreenArrow />}
-	    {children}
-	</a>
-    );
+  return btnClass === "button-primary" ? (
+    <button disabled={disabled} className={btnClass}>
+      {children}
+    </button>
+  ) : (
+    <a href={href} disabled={disabled} className={btnClass}>
+      {btnClass === "button-tertiary" && <GreenArrow />}
+      {children}
+    </a>
+  );
 }
