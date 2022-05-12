6c9a6f52b5acd7553b01b8c8c59334c77410f3e6
Pedro Fonseca <pmlfonseca@outlook.com>
2022-05-12 21:31:54 +0100

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,19 +10,19 @@ function Home() {
 
       <div class="gradient-zig-zag">
         <Button btnClass="button-primary">Primary</Button>
-		    <Button btnClass="button-primary" disabled>
-		      Primary disabled
-		    </Button>
-		    <Button btnClass="button-secondary">Secondary</Button>
-		    <Button btnClass="button-secondary" disabled>
-		      Secondary disabled
-		    </Button>
-		    <Button btnClass="button-tertiary">Tertiary</Button>
-		    <Button btnClass="button-tertiary" disabled>
-		      Tertiary disabled
-		    </Button>
-		  </div>
-	  </div>
+        <Button btnClass="button-primary" disabled>
+          Primary disabled
+        </Button>
+        <Button btnClass="button-secondary">Secondary</Button>
+        <Button btnClass="button-secondary" disabled>
+          Secondary disabled
+        </Button>
+        <Button btnClass="button-tertiary">Tertiary</Button>
+        <Button btnClass="button-tertiary" disabled>
+          Tertiary disabled
+        </Button>
+      </div>
+    </div>
   );
 }
 
