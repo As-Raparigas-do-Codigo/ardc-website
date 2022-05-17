8f447eb7b771f740d3080e717f6157a9d6b52943
MonicaFidalgo <monica.mofi@gmail.com>
2022-05-18 00:20:23 +0100

    feat(talks-events): add data files and images for talks events blog

---
+++ src/data/events.js
@@ -0,0 +1,36 @@
+import aubay from "../assets/events/aubay-mulheres-tecnologia.jpeg";
+
+const events = [
+  {
+    id: 1,
+    title: "Webinar Aubay Portugal(Women in Tech) ",
+    image: aubay,
+    alt: "Zambia elephants cartoon",
+    description:
+      "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.",
+    link: "Ver vídeo no YouTube",
+    url: "https://raparigasdocodigo.pt/atividades/",
+  },
+  {
+    id: 2,
+    title: "Webinar Aubay Portugal(Women in Tech) ",
+    image: aubay,
+    alt: "Zambia elephants cartoon",
+    description:
+      "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.",
+    link: "Ver vídeo no YouTube",
+    url: "https://raparigasdocodigo.pt/atividades/",
+  },
+  {
+    id: 3,
+    title: "Webinar Aubay Portugal(Women in Tech) ",
+    image: aubay,
+    alt: "Zambia elephants cartoon",
+    description:
+      "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.",
+    link: "Ver vídeo no YouTube",
+    url: "https://raparigasdocodigo.pt/atividades/",
+  },
+];
+
+export default events;
