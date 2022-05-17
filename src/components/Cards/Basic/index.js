fa9a5a5c8968cfeaf5c85b35901f991718d72c0e
MonicaFidalgo <monica.mofi@gmail.com>
2022-05-18 00:21:04 +0100

    feat(talks): add events and articles section

---
+++ src/components/Cards/Basic/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "../../Button";
+import "./basic-card.scss";
+
+function BasicCard(props) {
+  return (
+    <div className="basic-card" id={props.id}>
+      <img src={props.imgPath} className="basic-card__img" alt={props.alt} />
+      <div className="basic-card__body">
+        {props.date ? <span>{props.date}</span> : null}
+        <h5>{props.title}</h5>
+        <p className="basic-card__description">{props.description}</p>
+        <Button btnClass="button-tertiary" href={props.url}>
+          {props.link}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default BasicCard;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,13 +2,15 @@ import React from "react";
 
 // Components
 import Button from "../Button";
+import Events from "./components/EventsSection";
+import Articles from "./components/ArticlesSection";
 
 function Home() {
   return (
     <div className="home">
       <h1>Home Page</h1>
 
-      <div class="gradient-zig-zag">
+      <div className="gradient">
         <Button btnClass="button-primary">Primary</Button>
         <Button btnClass="button-primary" disabled>
           Primary disabled
@@ -21,6 +23,22 @@ function Home() {
         <Button btnClass="button-tertiary" disabled>
           Tertiary disabled
         </Button>
+      </div>
+
+      <div className="zig-zag">
+        <Events />
+      </div>
+
+      <div className="gradient">
+        <h2>Conhece os nossos mentores. -- to be done</h2>
+      </div>
+
+      <div className="zig-zag">
+        <Articles />
+      </div>
+
+      <div className="bg-pink">
+        <h2>Ajuda o projecto a crescer -- to be done</h2>
       </div>
     </div>
   );
