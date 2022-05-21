46063184448e1c9953918dcb0fb8fdb19a594ecb
MonicaFidalgo <monica.mofi@gmail.com>
2022-05-21 23:21:25 +0100

    feat(refactor): move pages to pages folder and change paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Workshops from "./components/Workshops";
-import Mentorship from "./components/Mentorship";
-import Events from "./components/Events";
-import About from "./components/About";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
-import NavBar from "./components/NavBarComponent";
-import Social from "./components/Social";
+import Home from "pages/Home";
+import Workshops from "pages/Workshops";
+import Mentorship from "pages/Mentorship";
+import Events from "pages/Events";
+import About from "pages/About";
+import Blog from "pages/Blog";
+import Contact from "pages/Contact";
+import NavBar from "components/NavBarComponent";
+import Social from "components/Social";
 
 function App() {
   return (
