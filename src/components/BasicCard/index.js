7f5217db7faa26e115aafcd93b49b09a670f47fb
MonicaFidalgo <monica.mofi@gmail.com>
2022-05-21 23:07:38 +0100

    feat(workshops): improve logic

--- src/components/BasicCard/index.js
+++ src/components/BasicCard/index.js
@@ -7,7 +7,7 @@ function BasicCard(props) {
     <div className="basic-card" id={props.id}>
       <img src={props.imgPath} className="basic-card__img" alt={props.alt} />
       <div className="basic-card__body">
-        {props.date ? <span>{props.date}</span> : null}
+        {props.date && <span>{props.date}</span>}
         <h5>{props.title}</h5>
         <p className="basic-card__description">{props.description}</p>
         <Button btnClass="button-tertiary" href={props.url}>
