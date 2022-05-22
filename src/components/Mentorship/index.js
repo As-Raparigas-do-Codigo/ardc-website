3292165eeb119aa69c5d601c27ba2dd0307fc8da
Inês Martins <in.afmartins@gmail.com>
2022-05-22 10:15:03 +0100

    adiciona formulario de contacto e mentoria as paginas

--- src/components/Mentorship/index.js
+++ src/components/Mentorship/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MentorshipForm from "../Forms/MentorshipForm";
 
 function Mentorship() {
   return (
     <div className="mentorship">
       <h1>Mentorship Page</h1>
+      <MentorshipForm/>
     </div>
   );
 }
