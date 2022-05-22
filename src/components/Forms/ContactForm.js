ce566d04ea522ecb170b921562f1d35b86aed34d
Nuno Faria <2145649+nunofaria11@users.noreply.github.com>
2022-05-22 18:23:52 +0100

    feat(contact): stylize contact form and page
    - add "btnType" property to Button component

--- src/components/Forms/ContactForm.js
+++ src/components/Forms/ContactForm.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Stack } from "react-bootstrap";
 
 function ContactForm() {
-  
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
-  
+
   const sendForm = (evt) => {
     evt.preventDefault()
     var myHeaders = new Headers();
@@ -26,31 +26,35 @@ function ContactForm() {
       redirect: 'follow'
     };
     fetch("https://apex.oracle.com/pls/apex/ardc/forms/contact", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
-  
+
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="nameInputField">
-        <Form.Control type="text" placeholder="Nome *" value={name} onChange={(e) => setName(e.target.value)}/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="emailInputField">
-        <Form.Control type="email" placeholder="Email *" value={email} onChange={(e) => setEmail(e.target.value)}/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="subjectInputField">
-        <Form.Control type="text" placeholder="Assunto *" value={subject} onChange={(e) => setSubject(e.target.value)}/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="subjectMessageField">
-        <Form.Control type="text" placeholder="Mensagem / Comentário" value={message} onChange={(e) => setMessage(e.target.value)}/>
-      </Form.Group>
-      <Button variant="primary" type="submit" disabled={!name || !email || !subject || !message} onClick={sendForm}>
-        Enviar mensagem
-      </Button>
+      <Stack gap={3}>
+        <Form.Group className="mb-3" controlId="nameInputField">
+          <Form.Control type="text" placeholder="Nome *" value={name} onChange={(e) => setName(e.target.value)} size="lg" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="emailInputField">
+          <Form.Control type="email" placeholder="Email *" value={email} onChange={(e) => setEmail(e.target.value)} size="lg" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="subjectInputField">
+          <Form.Control type="text" placeholder="Assunto *" value={subject} onChange={(e) => setSubject(e.target.value)} size="lg" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="subjectMessageField">
+          <Form.Control type="text" as="textarea" rows="3" placeholder="Mensagem / Comentário" value={message} onChange={(e) => setMessage(e.target.value)} size="lg" />
+        </Form.Group>
+      </Stack>
+      <div className="d-flex justify-content-between">
+        <p className="mandatory-hint">* Preenchimento obrigatório</p>
+        <Button variant="primary" type="submit" disabled={!name || !email || !subject || !message} onClick={sendForm}>
+          Enviar mensagem
+        </Button>
+      </div>
     </Form>
   );
 }
-  
+
 export default ContactForm;
-  
