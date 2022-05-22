e5e6408f0730164311ba2c4cda40661a348f2573
Inês Martins <in.afmartins@gmail.com>
2022-05-22 10:15:03 +0100

    adiciona formulario de contacto e mentoria as paginas

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ContactForm from "../Forms/ContactForm";
 
 function Contact() {
   return (
     <div className="Contact">
       <h1>Contact Page</h1>
+      <ContactForm/>
     </div>
   );
 }
