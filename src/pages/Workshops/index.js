c9ba496ad5d6d23b3d05394287a679499ea90972
Ines de Matos <itm.m92@gmail.com>
2022-06-02 16:40:25 +0100

    feature(breadcrumbs): breadcrumbs in all pages except for home

--- src/pages/Workshops/index.js
+++ src/pages/Workshops/index.js
@@ -3,8 +3,16 @@ import React from "react";
 import PageLayout from "components/PageLayout";
 
 function Workshops() {
+  const breadcrumbs = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    { label: "Workshops" },
+  ];
+
   return (
-    <PageLayout>
+    <PageLayout breadcrumbsData={breadcrumbs}>
       <h1>Workshops Page</h1>
     </PageLayout>
   );
