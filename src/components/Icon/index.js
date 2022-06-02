f97a089ca3cf71f44f9396815ca20c8a11705ddd
Inês Martins <in.afmartins@gmail.com>
2022-06-02 21:55:37 +0100

    [FEAT] adiciona lógica para barra de redes sociais

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,33 +1,58 @@
 import React from "react";
-import { ReactComponent as InstagramIcon } from "../../assets/icons/social/instagram.svg";
-import { ReactComponent as FacebookIcon } from "../../assets/icons/social/facebook.svg";
-import { ReactComponent as GithubIcon } from "../../assets/icons/social/github.svg";
+import { ReactComponent as InstagramDefaultIcon } from "../../assets/icons/social/instagram.svg";
+import { ReactComponent as InstagramTopBarIcon } from "../../assets/icons/social/instagram_white.svg";
+import { ReactComponent as FacebookDefaultIcon } from "../../assets/icons/social/facebook.svg";
+import { ReactComponent as FacebookTopBarIcon } from "../../assets/icons/social/facebook_white.svg";
+import { ReactComponent as LinkedinDefaultIcon } from "../../assets/icons/social/linkedin.svg";
+import { ReactComponent as LinkedinTopBarIcon } from "../../assets/icons/social/linkedin_white.svg";
+import { ReactComponent as GithubDefaultIcon } from "../../assets/icons/social/github.svg";
+import { ReactComponent as GithubTopBarIcon } from "../../assets/icons/social/github_white.svg";
 import { ReactComponent as TwitterIcon } from "../../assets/icons/social/twitter.svg";
-import { ReactComponent as LinkedinIcon } from "../../assets/icons/social/linkedin.svg";
 import { ReactComponent as MediumIcon } from "../../assets/icons/social/medium.svg";
 import { ReactComponent as Other } from "../../assets/icons/social/other.svg";
 import { ReactComponent as EmailIcon } from "../../assets/icons/email.svg";
 
-function Icon({ name, link = "#", onClickAction = null }) {
+function Icon({ name, link = "#", onClickAction = null, theme = "default" }) {
   const iconsMap = {
-    instagram: <InstagramIcon />,
-    facebook: <FacebookIcon />,
-    linkedin: <LinkedinIcon />,
-    twitter: <TwitterIcon />,
-    github: <GithubIcon />,
-    medium: <MediumIcon />,
-    email: <EmailIcon />,
-    other: <Other />,
+    instagram: {
+      default: <InstagramDefaultIcon />,
+      topBar: <InstagramTopBarIcon />
+    },
+    facebook: {
+      default: <FacebookDefaultIcon />,
+      topBar: <FacebookTopBarIcon />
+    },
+    linkedin: {
+      default: <LinkedinDefaultIcon />,
+      topBar: <LinkedinTopBarIcon />
+    },
+    github: {
+      default: <GithubDefaultIcon />,
+      topBar: <GithubTopBarIcon />
+    },
+    twitter: {
+      default: <TwitterIcon />
+    },
+    medium: {
+      default: <MediumIcon />
+    },
+    email: {
+      default: <EmailIcon />
+    },
+    other: {
+      default: <Other />
+    }
   };
 
-  const ClickableIcon = <span onClick={onClickAction}>{iconsMap[name]}</span>;
+  const icon = iconsMap[name][theme]
+  const ClickableIcon = <span onClick={onClickAction}>{icon}</span>;
   const LinkableIcon = (
     <a
-      className={"me-3 social-link-svg"}
+      className={ "me-3 social-link-svg"}
       href={link}
       target="_blank"
       rel="noreferrer">
-      {iconsMap[name]}
+      {icon}
     </a>
   );
 
