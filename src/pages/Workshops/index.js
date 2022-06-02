36a9e25fbd31f743c5c1b36b3695278037720440
Nuno Faria <2145649+nunofaria11@users.noreply.github.com>
2022-06-02 19:05:47 +0100

    fix: workshops menu

--- src/pages/Workshops/index.js
+++ src/pages/Workshops/index.js
@@ -4,7 +4,7 @@ import PageLayout from "components/PageLayout";
 
 function Workshops() {
   return (
-    <PageLayout>
+    <PageLayout title="Workshops" description="Oferecemos vários cursos e workshops de programação, totalmente gratuitos e adaptados às necessidades e interesses das alunas inscritas.">
       <h1>Workshops Page</h1>
     </PageLayout>
   );
