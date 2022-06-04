81403f3f57910505e52204136fdb24879918930e
Inês Martins <in.afmartins@gmail.com>
2022-06-04 13:20:50 +0100

    [FEAT] melhora textos da secção de missão

--- src/Constants.js
+++ src/Constants.js
@@ -38,7 +38,7 @@ const SiteContent = {
   Text: {
     Copyright: "Copyright © 2022 As Raparigas do Código. All Rights Reserved",
     WhoWeAre: "Uma organização focada em desmistificar o papel da mulher na tecnologia, promovendo a inclusão digital e encorajando mais raparigas e mulheres a ingressar no sector das tecnologias de informação.",
-    OurMission_p1: "É nossa missão garantir que todas as mulheres que queiram aprender sobre tecnologia tenham um espaço onde o possam fazer rodeadas de colegas e pares que as representam e apoiam.",
+    OurMission_p1: "Garantir que todas as mulheres que queiram aprender sobre tecnologia tenham um espaço onde o possam fazer rodeadas de colegas e pares que as representam e apoiam.",
     OurMission_p2: "Queremos desmistificar o papel da mulher na tecnologia, encorajando mais raparigas e mulheres a ingressar no sector das tecnologias de informação.",
     OurWorkshops: "Oferecemos vários cursos e workshops, totalmente gratuitos e adaptados às necessidades e interesses das alunas inscritas."
   }
