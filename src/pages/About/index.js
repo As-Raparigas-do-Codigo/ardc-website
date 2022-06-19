e6fdad75f3241f1b4bc83696059be387483db8d1
MonicaFidalgo <monica.mofi@gmail.com>
2022-06-19 22:19:20 +0100

    feat(about-us): add about us content

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-import PageLayout from "components/PageLayout";
 import Constants from "Constants";
+import { Container, Row, Col } from "react-bootstrap";
+
+// components
+import PageLayout from "components/PageLayout";
+import Teaser from "components/Teaser";
+import Statistics from "components/Statistics";
+
+// data
+import about from "data/about.js";
 
 function About() {
   const breadcrumbs = [
@@ -15,12 +23,49 @@ function About() {
     { label: "Sobre o projecto" },
   ];
 
+  const aboutBellow = about.slice(1, 3).map((data) => {
+    return (
+      <Col md={12} key={data.id}>
+        <Teaser
+          title={data.title}
+          subtitle={data.subtitle}
+          description={data.description}
+          imgPath={data.img}
+          imageRight={data.pictureRight}
+          linkFirst={data.link1}
+        />
+      </Col>
+    );
+  });
+
   return (
     <PageLayout
       title="Sobre o projecto"
       description="bla bla bla cenas"
-      breadcrumbsData={breadcrumbs}>
-      <h1>As Raparigas do Código</h1>
+      breadcrumbsData={breadcrumbs}
+    >
+      <Container>
+        <Row>
+          <Col md={12} key={about[0].id}>
+            <Teaser
+              title={about[0].title}
+              subtitle={about[0].subtitle}
+              description={about[0].description}
+              imgPath={about[0].img}
+              imageRight={about[0].pictureRight}
+              linkFirst={about[0].link1}
+            />
+          </Col>
+        </Row>
+      </Container>
+      <div className="zig-zag"></div>
+      <div className="bg-pink">
+        <Statistics />
+      </div>
+      <Container>
+        {" "}
+        <Row>{aboutBellow}</Row>
+      </Container>
     </PageLayout>
   );
 }
