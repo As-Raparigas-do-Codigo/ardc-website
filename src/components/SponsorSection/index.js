3cc59d0191472b0662b6f82f58afcef8df2dc1d1
Pedro Fonseca <pmlfonseca@outlook.com>
2022-08-12 00:11:35 +0100

    adiciona links, dados reais niveis aos sponsors

--- src/components/SponsorSection/index.js
+++ src/components/SponsorSection/index.js
@@ -1,37 +1,39 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-// assets
-import AWS from "assets/sponsorlogos/aws.png";
-import PortoTechHub from "assets/sponsorlogos/portohub.png";
-import WIT from "assets/sponsorlogos/wit.png";
 import "./sponsorSection.scss";
+import sponsors from "data/sponsors";
 
-const SponsorImages = [{
-  id: 1,
-  src: AWS,
-  alt: "AWS logo"
-}, {
-  id: 2,
-  src: PortoTechHub,
-  alt: "Porto Tech Hub logo"
-}, {
-  id: 3,
-  src: WIT,
-  alt: "Women In Tech logo"
-}];
+const SponsorSection = () => {
 
-const SponsorSection = () => (
-  <Container className="mt-5 pt-md-5">
-    <h6 className="sponsor-title text-uppercase text-center mb-4">Os nossos parceiros</h6>
-    <Row className="justify-content-center">
-      {SponsorImages.map(image => (
-        <Col key={image.id} className="col-auto py-3 my-md-5 mx-5">
-          <img className="sponsor-image" src={image.src} alt={image.alt} />
-        </Col>
-      ))}
+  const renderSponsorLogos = (level) => {
+    const hasGrayscale = [2, 3].includes(level)
+    const sponsorsToDisplay = sponsors.filter(
+      sponsor => sponsor.level === level
+    )
+
+    return <Row className="justify-content-center">
+      {
+        sponsorsToDisplay.map(
+          sponsor => (
+            <Col key={sponsor.id} className="col-auto py-3 my-md-2 mx-2">
+              <a href={sponsor.link}>
+                <img className={`${hasGrayscale ? "sponsor-grayscale" : null} sponsor-${level}`} src={sponsor.image} alt={sponsor.alt} />
+              </a>
+            </Col>
+          )
+        )
+      }
     </Row>
-  </Container>
-);
+  }
 
+  return (
+    <Container className="mt-5 pt-md-5 py-5">
+      <h6 className="sponsor-title text-uppercase text-center mb-4">Os nossos parceiros</h6>
+      {renderSponsorLogos(1)}
+      {renderSponsorLogos(2)}
+      {renderSponsorLogos(3)}
+    </Container>
+  )
+}
 
 export default SponsorSection;
---
+++ src/data/sponsors.js
@@ -0,0 +1,69 @@
+import aws from "../assets/sponsorlogos/aws.png"
+import wit from "../assets/sponsorlogos/wit.png"
+import portohub from "../assets/sponsorlogos/portohub.png"
+import microsoft from "../assets/sponsorlogos/microsoft.png"
+import teleperformance from "../assets/sponsorlogos/teleperformance.png"
+import outsystems from "../assets/sponsorlogos/outsystems.png"
+import valtech from "../assets/sponsorlogos/valtech.png"
+import kwan from "../assets/sponsorlogos/kwan.png"
+
+const sponsors = [
+	{
+		id: 1,
+		image: aws,
+		alt: "Logo da AWS",
+		level: 2,
+		link: "https://aws.amazon.com/",
+	},
+	{
+		id: 2,
+		image: wit,
+		alt: "Logo das Women In Tech",
+		level: 3,
+		link: "https://women-in-tech.org/",
+	},
+	{
+		id: 3,
+		image: portohub,
+		alt: "Logo do Porto Tech Hub",
+		level: 3,
+		link: "https://portotechhub.com/",
+	},
+	{
+		id: 4,
+		image: microsoft,
+		alt: "Logo da Microsoft",
+		level: 2,
+		link: "https://www.microsoft.com/pt-pt/",
+	},
+	{
+		id: 6,
+		image: teleperformance,
+		alt: "Logo da Teleperformance",
+		level: 2,
+		link: "https://jobs.teleperformance.pt/",
+	},
+	{
+		id: 7,
+		image: outsystems,
+		alt: "Logo da Outsystems",
+		level: 1,
+		link: "https://www.outsystems.com/",
+	},
+	{
+		id: 8,
+		image: valtech,
+		alt: "Logo da Valtech",
+		level: 2,
+		link: "https://www.valtech.com/",
+	},
+	{
+		id: 9,
+		image: kwan,
+		alt: "Logo da KWAN",
+		level: 3,
+		link: "https://kwan.pt/",
+	}
+];
+
+export default sponsors;
