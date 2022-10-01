5a08b27ff06b6a3fdc3103b3441d0d63af23e3fa
Inês <in.afmartins@gmail.com>
2022-10-01 14:40:59 +0100

    adiciona métricas reais

--- src/components/KPISSection/index.js
+++ src/components/KPISSection/index.js
@@ -6,15 +6,15 @@ const KPISSection = () => {
     <Container className="py-5 px-2">
       <Row>
         <Col className="px-5 my-2">
-          <h1 className="text-center fs-x5 fw-bold mb-1">123+</h1>
+          <h1 className="text-center fs-x5 fw-bold mb-1">67</h1>
           <p className="text-center">Workshops realizados</p>
         </Col>
         <Col className="px-5 my-2">
-          <h1 className="text-center fs-x5 fw-bold mb-1">45+</h1>
+          <h1 className="text-center fs-x5 fw-bold mb-1">25</h1>
           <p className="text-center">Professores & Mentores</p>
         </Col>
         <Col className="px-5 my-2">
-          <h1 className="text-center fs-x5 fw-bold mb-1">678</h1>
+          <h1 className="text-center fs-x5 fw-bold mb-1">400+</h1>
           <p className="text-center">Alunas inscritas</p>
         </Col>
       </Row>
