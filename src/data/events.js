a14c77d1971b97f2275999721d84fc80e4273fdc
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2022-11-06 20:20:56 +0000

    add community get together and aubady podcast

--- src/data/events.js
+++ src/data/events.js
@@ -1,35 +1,36 @@
 import aubay from '../assets/events/aubay-mulheres-tecnologia.jpeg';
+import techCommunityGetTogether from '../assets/events/tech-community-get-together.jpeg';
 
 const Events = [
   {
     id: 1,
-    title: 'Webinar Aubay Portugal(Women in Tech) ',
-    image: aubay,
-    alt: 'Zambia elephants cartoon',
+    title: 'Tech Community Get-Together',
+    image: techCommunityGetTogether,
+    alt: 'Evento presencial',
     description:
-      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.',
-    link: 'Ver vídeo no YouTube',
-    url: 'https://raparigasdocodigo.pt/atividades/'
+      'Um evento organizado por nós aberto à comunidade na escola 42 Lisboa que envolveu cerca de 60 pessoas.',
+    link: '',
+    url: ''
   },
   {
     id: 2,
-    title: 'Webinar Aubay Portugal(Women in Tech) ',
+    title: 'Mulheres na Tecnologia - Ouves-me Aubay?',
     image: aubay,
-    alt: 'Zambia elephants cartoon',
+    alt: 'Episódio sobre Mulheres na Tecnologia no podcast Ouves-me Aubay?',
     description:
-      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.',
-    link: 'Ver vídeo no YouTube',
-    url: 'https://raparigasdocodigo.pt/atividades/'
+      'Uma conversa informal sobre a temática das Mulheres na Tecnologia com participaçāo especial da equipa.',
+    link: 'Ouve o episódio de podcast',
+    url: 'https://open.spotify.com/episode/6Ey863AFPBtoGLSaaFMtEo?si=zz-UZ5W2RGS-ePW1ox7W5g&nd=1'
   },
   {
     id: 3,
-    title: 'Webinar Aubay Portugal(Women in Tech) ',
+    title: 'Webinar Aubay Portugal (Women in Tech)',
     image: aubay,
     alt: 'Zambia elephants cartoon',
     description:
       'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.',
-    link: 'Ver vídeo no YouTube',
-    url: 'https://raparigasdocodigo.pt/atividades/'
+    link: 'Vê o webinar no Youtube',
+    url: 'https://www.youtube.com/watch?v=ccV9BrBjz2s'
   }
 ];
 
