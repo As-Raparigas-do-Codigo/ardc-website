a88bbe6510f0c79f679f2597ec40ed42ac82b43b
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2022-11-06 20:55:41 +0000

    add more info on women in tech event

--- src/data/events.js
+++ src/data/events.js
@@ -1,12 +1,13 @@
 import aubay from '../assets/events/aubay-mulheres-tecnologia.jpeg';
 import techCommunityGetTogether from '../assets/events/tech-community-get-together.jpeg';
+import geekGirlsPortugalRumoIT from '../assets/events/g2pt93-geek-girls-portugal.jpeg';
 
 const Events = [
   {
     id: 1,
     title: 'Tech Community Get-Together',
     image: techCommunityGetTogether,
-    alt: 'Evento presencial',
+    alt: 'Evento presencial organizado por nós',
     description:
       'Um evento organizado por nós aberto à comunidade na escola 42 Lisboa que envolveu cerca de 60 pessoas.',
     link: '',
@@ -14,21 +15,21 @@ const Events = [
   },
   {
     id: 2,
-    title: 'Mulheres na Tecnologia - Ouves-me Aubay?',
-    image: aubay,
-    alt: 'Episódio sobre Mulheres na Tecnologia no podcast Ouves-me Aubay?',
+    title: '93º Geek Girls Portugal - Rumo a uma carreira de TI',
+    image: geekGirlsPortugalRumoIT,
+    alt: 'Evento online organizado por Geek Girls Portugal',
     description:
-      'Uma conversa informal sobre a temática das Mulheres na Tecnologia com participaçāo especial da equipa.',
-    link: 'Ouve o episódio de podcast',
-    url: 'https://open.spotify.com/episode/6Ey863AFPBtoGLSaaFMtEo?si=zz-UZ5W2RGS-ePW1ox7W5g&nd=1'
+      'Participamos no evento 93º Geek Girls Portugal para discutir sobre Rumo a uma carreira em TI.',
+    link: 'Vê o apresentação no Youtube',
+    url: 'https://www.youtube.com/watch?v=f7G9geDLWSw'
   },
   {
     id: 3,
-    title: 'Webinar Aubay Portugal (Women in Tech)',
+    title: 'Webinar Aubay Portugal - Women in Tech',
     image: aubay,
-    alt: 'Zambia elephants cartoon',
+    alt: 'Webinar Aubay Portugal - Women in Tech',
     description:
-      'Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Morbi sit amet tellus ut ligula egestas.',
+      'Estivemos presentes no webinar da Aubay Portugal sobre mulheres em tecnologia.',
     link: 'Vê o webinar no Youtube',
     url: 'https://www.youtube.com/watch?v=ccV9BrBjz2s'
   }
