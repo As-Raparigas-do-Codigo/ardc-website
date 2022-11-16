616b46c3444f442d2152880bf8d229b39b737535
Inês <in.afmartins@gmail.com>
2022-11-16 19:09:31 +0000

    [feat] changes location to make sure routes are ok

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Workshops from 'pages/Workshops';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBarComponent';
 import Social from 'components/Social';
-import SiteRoutes from 'data/Routes';
 import translationsEN from 'data/locales/translations_en.json';
 import translationsPT from 'data/locales/translations_pt.json';
 
@@ -37,9 +36,35 @@ function App() {
     if (currentLang == 'pt') {
       i18n.changeLanguage('en')
       setLang('en')
+      if (location.pathname == '/sobre') {
+        location.pathname = '/about'
+      } else if (location.pathname == '/contactos') {
+        location.pathname = '/contacts'
+      } else if (location.pathname == '/eventos') {
+        location.pathname = '/events'
+      } else if (location.pathname == '/mentorias') {
+        location.pathname = '/mentorships'
+      } else if (location.pathname == '/privacidade') {
+        location.pathname = '/privacy'
+      } else if (location.pathname == '/equipa') {
+        location.pathname = '/team'
+      }
     } else {
       i18n.changeLanguage('pt')
       setLang('pt')
+      if (location.pathname == '/about') {
+        location.pathname = '/sobre'
+      } else if (location.pathname == '/contacts') {
+        location.pathname = '/contactos'
+      } else if (location.pathname == '/events') {
+        location.pathname = '/eventos'
+      } else if (location.pathname == '/mentorships') {
+        location.pathname = '/mentorias'
+      } else if (location.pathname == '/privacy') {
+        location.pathname = '/privacidade'
+      } else if (location.pathname == '/team') {
+        location.pathname = '/equipa'
+      }
     }
   }
   return (
@@ -50,14 +75,20 @@ function App() {
           <NavBar translation={t} />
         </header>
         <Routes>
-          <Route exact path={SiteRoutes(t).About} element={<AboutUs translation={t}/>} />
-          <Route exact path={SiteRoutes(t).Contacts} element={<Contact translation={t}/>} />
-          <Route exact path={SiteRoutes(t).Events} element={<Events translation={t}/>} />
-          <Route exact path={SiteRoutes(t).Home} element={<Home translation={t}/>} />
-          <Route exact path={SiteRoutes(t).Mentorships} element={<Mentorship translation={t}/>} />
-          <Route exact path={SiteRoutes(t).PrivacyPolicy} element={<PrivacyPolicy translation={t}/>} />
-          <Route exact path={SiteRoutes(t).Team} element={<Team translation={t}/>} />
-          <Route exact path={SiteRoutes(t).Workshops} element={<Workshops translation={t}/>} />
+          <Route exact path='/about' element={<AboutUs translation={t}/>} />
+          <Route exact path='/sobre' element={<AboutUs translation={t}/>} />
+          <Route exact path='/contacts' element={<Contact translation={t}/>} />
+          <Route exact path='/contactos' element={<Contact translation={t}/>} />
+          <Route exact path='/events' element={<Events translation={t}/>} />
+          <Route exact path='/eventos' element={<Events translation={t}/>} />
+          <Route exact path='/' element={<Home translation={t}/>} />
+          <Route exact path='/mentorships' element={<Mentorship translation={t}/>} />
+          <Route exact path='/mentorias' element={<Mentorship translation={t}/>} />
+          <Route exact path='/privacy' element={<PrivacyPolicy translation={t}/>} />
+          <Route exact path='/privacidade' element={<PrivacyPolicy translation={t}/>} />
+          <Route exact path='/team' element={<Team translation={t}/>} />
+          <Route exact path='/equipa' element={<Team translation={t}/>} />
+          <Route exact path='/workshops' element={<Workshops translation={t}/>} />
         </Routes>
         <Footer translation={t} />
       </Router>
