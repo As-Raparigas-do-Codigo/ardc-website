2ae54b315025346d71d9f36a7e55e0041588c68c
Ines Matos <itm.m92@gmail.com>
2023-03-08 21:14:09 +0000

    fix: lint linkarrow component

--- src/components/LinkArrow/index.js
+++ src/components/LinkArrow/index.js
@@ -3,11 +3,17 @@ import { ReactComponent as GreenArrow } from 'assets/icons/green-arrow.svg';
 import { Col, Container, Row } from 'react-bootstrap';
 
 const LinkArrow = ({ href, disabled = false, download = false, children }) => (
-  <a className="button-tertiary" href={href} disabled={disabled} download={download}>
+  <a
+    className="button-tertiary"
+    href={href}
+    disabled={disabled}
+    download={download}
+    rel="noreferrer"
+    target="_blank">
     <Container>
       <Row>
         <Col className="col-auto p-0">
-          <GreenArrow className="arrow-size-default"/>
+          <GreenArrow className="arrow-size-default" />
         </Col>
         <Col className="col-auto">{children}</Col>
       </Row>
