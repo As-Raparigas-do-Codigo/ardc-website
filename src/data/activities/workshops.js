bdc0c496d40d45c3cd1d05e4158c9efdc18f74a3
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2023-04-05 20:51:51 +0100

    removed commented workshops

--- src/data/activities/workshops.js
+++ src/data/activities/workshops.js
@@ -9,56 +9,6 @@
 
 import Constants from 'constants';
 
-// const WorkshopsData = [
-//   {
-//     id: 1,
-//     title: 'React',
-//     image: react,
-//     alt: 'React workshop',
-//     description:
-//       'Temos o prazer de apresentar a primeira edição do workshop de React. Esta biblioteca criada pela Meta (antiga Facebook) tem vindo a crescer imenso na popularidade e na procura no mercado das TI.',
-//     descriptiveDate: 'Próxima Edição: 02 de Abril a 07 de Maio (Sábados), entre 10h e as 12h',
-//     startingDate: '2022-09-02T10:00:00Z',
-//     endingDate: '2022-10-17T12:00:00Z',
-//     linkUrl: 'https://raparigasdocodigo.pt/atividades/',
-//     buttonUrl: 'https://raparigasdocodigo.pt/atividades/',
-//     descriptiveDuration: 'Longa Duração',
-//     sessions: {
-//       total: 5,
-//       data: [
-//         {
-//           date: '2022-04-02T10:00:00Z',
-//           hour: '10h',
-//           duration: '2h'
-//         },
-//         {
-//           date: '2022-04-09T10:00:00Z',
-//           hour: '10h',
-//           duration: '2h'
-//         }
-//       ]
-//     }
-//   },
-//   {
-//     id: 8,
-//     title: 'Gestão de Carreira (Edição 2)',
-//     image: gestaocarreira2,
-//     alt: 'Workshop Gestão de Carreira (Edição 2)',
-//     description:
-//       'Uma carreira em Tech pode parecer algo assustador à primeira vista, \
-//       mas este workshop é mesmo para as desmistificar! \
-//       Vais perceber como procurar a vaga que pretendes, como te candidatares, \
-//       como são as entrevistas os Recursos Humanos e até as entrevistas técnicas! \
-//       Três sessões para esclareceres todas as tuas dúvidas.',
-//     descriptiveDate: null,
-//     startingDate: '2022-10-23T10:00:00Z',
-//     endingDate: null,
-//     linkUrl: 'https://raparigasdocodigo.pt/atividades/',
-//     buttonUrl: 'https://raparigasdocodigo.pt/atividades/',
-//     descriptiveDuration: 'Curta Duração'
-//   }
-// ];
-
 const Workshops = [
   {
     id: self.crypto.randomUUID(),
