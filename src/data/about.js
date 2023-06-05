643f841d531fbcb10918aab1bacdf70ae5e3fa1c
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2023-06-05 23:15:29 +0100

    Update about us copy (without images) (#92)

--- src/data/about.js
+++ src/data/about.js
@@ -6,25 +6,25 @@ const AboutData = (translation) => {
   return [
     {
       id: 1,
-      title: translation('AboutUsPage-Mission-Title'),
-      subtitle: translation('AboutUsPage-Mission-Subtitle'),
-      description: translation('AboutUsPage-Mission-Description'),
+      title: translation('AboutUsPage-Row1-Title'),
+      subtitle: translation('AboutUsPage-Row1-Subtitle'),
+      description: translation('AboutUsPage-Row1-Description'),
       img: image1,
       pictureRight: true
     },
     {
       id: 2,
-      title: translation('AboutUsPage-Goals-Title'),
-      subtitle: translation('AboutUsPage-Goals-Subtitle'),
-      description: translation('AboutUsPage-Goals-Description'),
+      title: translation('AboutUsPage-Row2-Title'),
+      subtitle: translation('AboutUsPage-Row2-Subtitle'),
+      description: translation('AboutUsPage-Row2-Description'),
       img: image2,
       pictureRight: false
     },
     {
       id: 3,
-      title: translation('AboutUsPage-HowWeWork-Title'),
-      subtitle: translation('AboutUsPage-HowWeWork-Subtitle'),
-      description: translation('AboutUsPage-HowWeWork-Description'),
+      title: translation('AboutUsPage-Row3-Title'),
+      subtitle: translation('AboutUsPage-Row3-Subtitle'),
+      description: translation('AboutUsPage-Row3-Description'),
       img: image3,
       pictureRight: true
     }
