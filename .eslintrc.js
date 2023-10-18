02dbcba5bb2082d5f515b03caec83ee7ecfd0139
Julia Mendes <juliamendesc@gmail.com>
2023-10-18 16:36:59 +0100

    add husky upon commiting staged files and precommit script to run on all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -16,6 +22,12 @@ module.exports = {
     'react/prop-types': 0,
     'react/no-unescaped-entities': 'warn',
     'no-undef': 0,
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    'prettier/prettier': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 };
