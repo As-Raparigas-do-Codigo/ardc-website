d1b40b6d99bbf25f71a07d7bd1e33093780d9e0b
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2024-01-29 20:01:49 +0000

    Update sponsors on homepage (#183)

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,70 +1,38 @@
-import aws from '../assets/sponsorlogos/aws.png';
 import wit from '../assets/sponsorlogos/wit.png';
-import portohub from '../assets/sponsorlogos/portohub.png';
 import microsoft from '../assets/sponsorlogos/microsoft.png';
-import teleperformance from '../assets/sponsorlogos/teleperformance.png';
 import outsystems from '../assets/sponsorlogos/outsystems.png';
-import valtech from '../assets/sponsorlogos/valtech.png';
-import kwan from '../assets/sponsorlogos/kwan.png';
+import nunocubalreis from '../assets/sponsorlogos/nunocubalreis.png';
 
 const amountOfTiers = 3;
 
 const sponsors = [
   {
     id: 1,
-    image: aws,
-    alt: 'Logo da AWS',
-    tier: 2,
-    link: 'https://aws.amazon.com/'
-  },
-  {
-    id: 2,
-    image: wit,
-    alt: 'Logo das Women In Tech',
-    tier: 3,
-    link: 'https://women-in-tech.org/'
-  },
-  {
-    id: 3,
-    image: portohub,
-    alt: 'Logo do Porto Tech Hub',
-    tier: 3,
-    link: 'https://portotechhub.com/'
-  },
-  {
-    id: 4,
-    image: microsoft,
-    alt: 'Logo da Microsoft',
-    tier: 2,
-    link: 'https://www.microsoft.com/pt-pt/'
-  },
-  {
-    id: 6,
-    image: teleperformance,
-    alt: 'Logo da Teleperformance',
-    tier: 2,
-    link: 'https://jobs.teleperformance.pt/'
-  },
-  {
-    id: 7,
     image: outsystems,
     alt: 'Logo da Outsystems',
     tier: 1,
     link: 'https://www.outsystems.com/'
   },
   {
-    id: 8,
-    image: valtech,
-    alt: 'Logo da Valtech',
+    id: 2,
+    image: microsoft,
+    alt: 'Logo da Microsoft',
     tier: 2,
-    link: 'https://www.valtech.com/'
+    link: 'https://www.microsoft.com/pt-pt/'
   },
   {
-    id: 9,
-    image: kwan,
-    alt: 'Logo da KWAN',
-    tier: 3,
-    link: 'https://kwan.pt/'
+    id: 3,
+    image: wit,
+    alt: 'Logo das Women In Tech',
+    tier: 2,
+    link: 'https://women-in-tech.org/'
+  },
+  {
+    id: 4,
+    image: nunocubalreis,
+    alt: 'Logo da Nuno Cubal Reis',
+    tier: 2,
+    link: 'https://www.nunoreis.pt/'
   }
 ];
 
