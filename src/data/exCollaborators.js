d6a181763a18c4baaae62bf74e976fa568692375
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2024-11-22 00:19:12 +0000

    move collaborators to ex collab

--- src/data/exCollaborators.js
+++ src/data/exCollaborators.js
@@ -68,7 +68,28 @@ const ExCollaborators = [
       { linkedin: 'https://www.linkedin.com/in/jessicalemos9/' },
       { github: 'https://github.com/jessicalemos ' }
     ]
-  }
+  },
+  {
+    name: 'Catarina Silva',
+    social: [
+      { linkedin: 'https://www.linkedin.com/in/catarina-ns-silva/' },
+      { github: 'https://github.com/CatarinaNSSilva' }
+    ],
+  },
+  {
+    name: 'Gustavo Rodrigues',
+    social: [
+      { linkedin: 'https://www.linkedin.com/in/gustavocrodrigues/' },
+      { github: 'https://github.com/gustavorodrigues19' }
+    ]
+  },
+  {
+    name: 'Cristiane Fidelix',
+    social: [
+      { linkedin: 'https://www.linkedin.com/in/cristianefidelix/' },
+      { instagram: 'https://www.instagram.com/cristianefidelix.live' }
+    ],
+  },
 ];
 
 export default ExCollaborators;
--- src/data/team.js
+++ src/data/team.js
@@ -56,18 +56,6 @@ const TeamData = [
     avatar: 'isabel.jpg'
   },
   {
-    id: 13,
-    roles: ['mentor', 'collaborator'],
-    name: 'Catarina Silva',
-    job: 'Researcher',
-    location: 'Coimbra',
-    social: [
-      { linkedin: 'https://www.linkedin.com/in/catarina-ns-silva/' },
-      { github: 'https://github.com/CatarinaNSSilva' }
-    ],
-    avatar: 'catarina-silva.png'
-  },
-  {
     id: 17,
     roles: ['mentor', 'collaborator'],
     name: 'Inês de Matos',
@@ -135,18 +123,6 @@ const TeamData = [
     avatar: 'ines-calcoa.jpeg'
   },
   {
-    id: 26,
-    roles: ['mentor', 'collaborator'],
-    name: 'Cristiane Fidelix',
-    job: 'IT Consultant - Cybersecurity Specialist',
-    location: 'Brasil',
-    social: [
-      { linkedin: 'https://www.linkedin.com/in/cristianefidelix/' },
-      { instagram: 'https://www.instagram.com/cristianefidelix.live' }
-    ],
-    avatar: 'cristiane-fidelix.jpeg'
-  },
-  {
     id: 27,
     roles: ['mentor', 'collaborator'],
     name: 'Teresa Alves',
@@ -157,18 +133,6 @@ const TeamData = [
       { github: 'https://github.com/teresalves' }
     ],
     avatar: 'teresa-alves.jpg'
-  },
-  {
-    id: 28,
-    roles: ['mentor', 'collaborator'],
-    name: 'Gustavo Rodrigues',
-    job: 'Full Stack Developer',
-    location: 'Lisboa',
-    social: [
-      { linkedin: 'https://www.linkedin.com/in/gustavocrodrigues/' },
-      { github: 'https://github.com/gustavorodrigues19' }
-    ],
-    avatar: 'gustavo-rodrigues.png'
   }
 ];
 
